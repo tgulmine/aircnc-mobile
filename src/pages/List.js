dbf53dc5609b85a0f759048680afd51ef1774a97
tgulmine <tgulmine@gmail.com>
2020-01-22 04:26:44 -0300

    first commit

---
+++ src/pages/List.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, Image, AsyncStorage } from 'react-native';
+
+import SpotList from '../components/SpotList';
+
+import logo from '../assets/logo.png'
+
+export default function List() {
+    const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storagedTechs => {
+            const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+
+            setTechs(techsArray);
+        })
+    }, [])
+
+    return <SafeAreaView style={styles.container}>
+        <Image style={styles.logo} source={logo} />
+
+        <ScrollView>
+            {techs.map(tech => <SpotList key={tech} tech={tech} />)}
+        </ScrollView>
+    </SafeAreaView>
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 30,
+    },
+
+    logo: {
+        height: 32,
+        resizeMode: 'contain',
+        alignSelf: 'center',
+        marginTop: 20,
+    }
+});
